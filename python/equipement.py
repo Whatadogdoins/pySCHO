18a2f4ed148b5bb4ad05b30a9c401a3d3bd9ea15
Whatadogdoins <119882676+Whatadogdoins@users.noreply.github.com>
2023-03-06 15:24:53 +0100

    Update equipement.py

--- python/equipement.py
+++ python/equipement.py
@@ -1,12 +1,17 @@
-equipments = [
+Armes = [
     {'name': 'Épée de pierre', 'attaque': 15, 'défense': 10},
     {'name': 'Arc long', 'attaque': 15, 'défense': 0},
     {'name': 'Bouclier en bois', 'attaque': 2, 'défense': 20},
     {'name': 'Hache de guerre en pierre', 'attaque': 20, 'défense': 5},
     {'name': 'Bâton de mage basique', 'attaque': 18, 'défense': 0}
-    {'name': 'Casque en cuir', 'attaque': 0, 'défense': 10}
-    {'name': 'Plastron en cuir', 'attaque': 0, 'défense': 15}
-    {'name': 'Gants en cuir', 'attaque': 0, 'défense': 5}
+
+]
+Armures = [
+    {"name": "Casque en cuir", "protection": 10, "résistance_feu": 0, "résistance_froid": 5},
+    {"name": "Plastron en cuir", "protection": 15, "résistance_feu": 0, "résistance_froid": 5},
+    {"name": "Gants en cuir", "protection": 5, "résistance_feu": 0, "résistance_froid": 5},
+    {"name": "Jambières en cuir", "protection": 10, "résistance_feu": 0, "résistance_froid": 5},
+    {"name": "Bottes en cuir", "protection": 5, "résistance_feu": 0, "résistance_froid": 5}
 ]
 #il faut mettre la liste des équipement avec leurs modificateur de stats
 #et les rassemblé quand le joueur les équipe
