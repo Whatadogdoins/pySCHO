5a0cf12566de80234324ac2e2000e8a5c7d46f2e
Whatadogdoins <119882676+Whatadogdoins@users.noreply.github.com>
2023-03-06 17:17:52 +0100

    Update equipement.py

--- python/equipement.py
+++ python/equipement.py
@@ -4,19 +4,23 @@ Armes = [
     {'name': 'Bouclier en bois', 'attaque': 2, 'défense': 20},
     {'name': 'Hache de guerre en pierre', 'attaque': 20, 'défense': 5},
     {'name': 'Bâton de mage basique', 'attaque': 18, 'défense': 0}
-
+    {'name': 'Bouclier en renforcé', 'attaque': 4, 'défense': 30},
+    
 ]
 Armures = [
+    {"name": "
     {"name": "Casque en cuir", "protection": 10, "résistance_chaud": 0, "résistance_froid": 5},
     {"name": "Plastron en cuir", "protection": 15, "résistance_chaud": 0, "résistance_froid": 5},
     {"name": "Gants en cuir", "protection": 5, "résistance_chaud": 0, "résistance_froid": 5},
     {"name": "Jambières en cuir", "protection": 10, "résistance_chaud": 0, "résistance_froid": 5},
     {"name": "Bottes en cuir", "protection": 5, "résistance_chaud": 0, "résistance_froid": 5}
-    {"name": "Casque en fer", "protection": 15, "résistance_chaud": -5, "résistance_froid": 0},
-    {"name": "Plastron en fer", "protection": 20, "résistance_chaud": -5, "résistance_froid": 0},
-    {"name": "Gants en fer", "protection": 10, "résistance_chaud": -5, "résistance_froid": 0},
-    {"name": "Jambières en fer", "protection": 15, "résistance_chaud": -5, "résistance_froid": 0},
-    {"name": "Bottes en fer", "protection": 10, "résistance_chaud": -5, "résistance_froid": 0}
+    {"name": "Casque en fer", "protection": 15, "résistance_chaud": 0, "résistance_froid": 0},
+    {"name": "Plastron en fer", "protection": 20, "résistance_chaud": 0, "résistance_froid": 0},
+    {"name": "Gants en fer", "protection": 10, "résistance_chaud": 0, "résistance_froid": 0},
+    {"name": "Jambières en fer", "protection": 15, "résistance_chaud": 0, "résistance_froid": 0},
+    {"name": "Bottes en fer", "protection": 10, "résistance_chaud": 0, "résistance_froid": 0}
 ]
+Artefact = [
+    {"name": "
 #il faut mettre la liste des équipement avec leurs modificateur de stats
 #et les rassemblé quand le joueur les équipe
