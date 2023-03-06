6fcb538146014890a5bbf74e373d7c211d10c716
Whatadogdoins <119882676+Whatadogdoins@users.noreply.github.com>
2023-03-06 15:46:35 +0100

    Update equipement.py

--- python/equipement.py
+++ python/equipement.py
@@ -7,11 +7,16 @@ Armes = [
 
 ]
 Armures = [
-    {"name": "Casque en cuir", "protection": 10, "résistance_feu": 0, "résistance_froid": 5},
-    {"name": "Plastron en cuir", "protection": 15, "résistance_feu": 0, "résistance_froid": 5},
-    {"name": "Gants en cuir", "protection": 5, "résistance_feu": 0, "résistance_froid": 5},
-    {"name": "Jambières en cuir", "protection": 10, "résistance_feu": 0, "résistance_froid": 5},
-    {"name": "Bottes en cuir", "protection": 5, "résistance_feu": 0, "résistance_froid": 5}
+    {"name": "Casque en cuir", "protection": 10, "résistance_chaud": 0, "résistance_froid": 5},
+    {"name": "Plastron en cuir", "protection": 15, "résistance_chaud": 0, "résistance_froid": 5},
+    {"name": "Gants en cuir", "protection": 5, "résistance_chaud": 0, "résistance_froid": 5},
+    {"name": "Jambières en cuir", "protection": 10, "résistance_chaud": 0, "résistance_froid": 5},
+    {"name": "Bottes en cuir", "protection": 5, "résistance_chaud": 0, "résistance_froid": 5}
+    {"name": "Casque en fer", "protection": 15, "résistance_chaud": -5, "résistance_froid": 0},
+    {"name": "Plastron en fer", "protection": 20, "résistance_chaud": -5, "résistance_froid": 0},
+    {"name": "Gants en fer", "protection": 10, "résistance_chaud": -5, "résistance_froid": 0},
+    {"name": "Jambières en fer", "protection": 15, "résistance_chaud": -5, "résistance_froid": 0},
+    {"name": "Bottes en fer", "protection": 10, "résistance_chaud": -5, "résistance_froid": 0}
 ]
 #il faut mettre la liste des équipement avec leurs modificateur de stats
 #et les rassemblé quand le joueur les équipe
