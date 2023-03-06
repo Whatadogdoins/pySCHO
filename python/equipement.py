4c4eeaf79c9d01fe8d12f9888f2937fb068cd743
Whatadogdoins <119882676+Whatadogdoins@users.noreply.github.com>
2023-03-06 15:13:22 +0100

    Update equipement.py

--- python/equipement.py
+++ python/equipement.py
@@ -1,2 +1,12 @@
+equipments = [
+    {'name': 'Épée de pierre', 'attaque': 15, 'défense': 10},
+    {'name': 'Arc long', 'attaque': 15, 'défense': 0},
+    {'name': 'Bouclier en bois', 'attaque': 2, 'défense': 20},
+    {'name': 'Hache de guerre en pierre', 'attaque': 20, 'défense': 5},
+    {'name': 'Bâton de mage basique', 'attaque': 18, 'défense': 0}
+    {'name': 'Casque en cuir', 'attaque': 0, 'défense': 10}
+    {'name': 'Plastron en cuir', 'attaque': 0, 'défense': 15}
+    {'name': 'Gants en cuir', 'attaque': 0, 'défense': 5}
+]
 #il faut mettre la liste des équipement avec leurs modificateur de stats
 #et les rassemblé quand le joueur les équipe
